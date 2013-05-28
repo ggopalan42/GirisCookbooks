bf8d5a04c7bc1f0237ab623b4664934afa308830
Giri Gopalan <ggopalan@yahoo.com>
2013-05-28 06:40:10 -0700

    Changed recipe to use the /tmp dir for the script

--- vtun/recipes/config-start-vtun.rb
+++ vtun/recipes/config-start-vtun.rb
@@ -1,8 +1,8 @@
-cookbook_file "install-vtund.sh" do
+cookbook_file "/tmp/install-vtund.sh" do
   source "install-vtund.sh"
   mode 0755
 end
 
 execute "install vtund" do
-  command "sh install-vtund.sh"
+  command "sh /tmp/install-vtund.sh"
 end
